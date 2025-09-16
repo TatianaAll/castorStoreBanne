5112f7362f7c0fb512e2dbd410fedf5bce2ccf90
TatianaAll <tatiana.allery@orange.fr>
2025-09-16 14:55:36 +0200

    feat: setup before coding

---
+++ src/Entity/Basket.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BasketRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BasketRepository::class)]
+class Basket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'basket', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
+
+    /**
+     * @var Collection<int, Product>
+     */
+    #[ORM\ManyToMany(targetEntity: Product::class)]
+    private Collection $product_id;
+
+    public function __construct()
+    {
+        $this->product_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProductId(): Collection
+    {
+        return $this->product_id;
+    }
+
+    public function addProductId(Product $productId): static
+    {
+        if (!$this->product_id->contains($productId)) {
+            $this->product_id->add($productId);
+        }
+
+        return $this;
+    }
+
+    public function removeProductId(Product $productId): static
+    {
+        $this->product_id->removeElement($productId);
+
+        return $this;
+    }
+}
