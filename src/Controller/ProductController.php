c20710620038fd0cc9ee58820cc0d5ede5c926d7
TatianaAll <tatiana.allery@orange.fr>
2025-09-16 15:50:44 +0200

    feat: create product Form + controller

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends AbstractController {
+  public function createProduct(Request $request, EntityManagerInterface $entityManager): Response {
+    //new instance of product
+    $product = new Product();
+
+    //make:form ProductType before, to create a form based on the entity Product
+    //formCreateProduct is an instance of the form create with productType
+    $formCreateProduct = $this->createForm(ProductType::class, $product);
+    //with handle request we get the POST request and interprete
+    $formCreateProduct->handleRequest($request);
+
+    if ($formCreateProduct->isSubmitted() && $formCreateProduct->isValid()) {
+      //entity manager is the ORM manager and permit to persist then flush in DB
+      $entityManager->persist($product);
+      $entityManager->flush();
+      
+      $this->addFlash('success', "Nouveau produit enregistré");
+      return $this->redirectToRoute('admin_dashboard');
+    }
+    $formView = $formCreateProduct->createView();
+    return $this->render('admin/product/create.html.twig', ["formView"=>$formView]);
+
+  }
+}
