1aad78f90f2b7c8a1e7bbf84e063704212a5d6d4
TatianaAll <tatiana.allery@orange.fr>
2025-09-17 15:02:39 +0200

    feat: correction bug liste users

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HomeController extends AbstractController
 {
@@ -22,20 +23,14 @@ class HomeController extends AbstractController
             'products'      => $products,
         ]);
     }
-}
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
-
-class HomeController extends AbstractController
-{
+    /*
     #[Route('/', name: 'app_home')]
-    public function index(UserInterface $user = null): Response
+    public function index2(UserInterface $user = null): Response
     {
         // $user est null si personne n'est connecté
         return $this->render('home/index.html.twig', [
             'user' => $user,
         ]);
     }
+        */
 }
