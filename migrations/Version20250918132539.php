52aaf11029d5fc56d974c19deb82f6985e961d49
h.amiotmagne <hugo463324@gmail.com>
2025-09-18 16:34:15 +0200

    css register

--- migrations/Version20250918132539.php
+++ migrations/Version20250918132539.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250918120720 extends AbstractMigration
+final class Version20250918132539 extends AbstractMigration
 {
     public function getDescription(): string
     {
